eab9947fa9b781f2d08969c1ccb466ade6555038
anmolhoney <anmolhoney2010@gmail.com>
2016-08-22 13:07:51 +0500

    sweet alerts added

--- blankApp/www/js/app.js
+++ blankApp/www/js/app.js
@@ -40,11 +40,11 @@
     views: {
       'menuContent': {
         templateUrl: 'search.html',
-           controller: 'MyCtrl'
+         
       }
     }
   })
-
+   
   .state('app.notif', {
       url: '/notif',
       views: {
@@ -80,26 +80,30 @@
     templateUrl: 'profile.html',
       controller: 'ProfileCtrl'
   })
-  .state('app.brow', {
-      url: '/brow',
-      views: {
-        'menuContent': {
-          templateUrl: 'brow.html',
-            controller: 'BrowCtrl'
-         
-        }
-      }
-    })
-   
+
+//  .state('app.brow', {
+//      url: '/brow',
+//      views: {
+//        'menuContent': {
+//          templateUrl: 'brow.html',
+//            controller: 'BrowCtrl'
+//         
+//        }
+//      }
+//    })
   .state('app.single', {
-    url: '/playlists/:playlistId',
+    url: '/profile/:profileId',
     views: {
       'menuContent': {
-        templateUrl: 'playlist.html',
-        controller: 'PlaylistCtrl'
+        templateUrl: 'profile.html',
+        controller: 'ProfileCtrl'
       }
     }
   });
+
+   
+   
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
 });
